e49173f94b24a69fe028a16de20a9eb22f98aee9
mARCIN_Dell <Dell@MARCIN_DELL_747>
2016-11-23 21:28:58 +0100

    Wrzucilem baze danych na podstawie twoich skryptow i dodalem projekt w mvc

---
+++ SBD_Project/SBD_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SBD_Project.Startup))]
+namespace SBD_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
